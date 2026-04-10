60d6d9ea3a7d60eb23005995647e0d134b8ee428
Mrxraes <brunoyano52@gmail.com>
2026-04-10 15:19:07 -0300

    Update teste.py

--- PROJETOjogoDaForca/teste.py
+++ PROJETOjogoDaForca/teste.py
@@ -1,9 +1,1 @@
-array = [{"nome": "bruno", "idade": 20}, {"nome": "luis", "idade": 53}, {"nome": "moises", "idade": 34}]
 
-nome = "bruno"
-
-for i in range(len(array)):
-    if nome in array[i]:
-        print(array["nome"])
-        array["nome"] += 20 
-        print(array["idade"])
